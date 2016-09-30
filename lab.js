112029e76a9750615d810648f11cb0d23eab36cb
Roan Wolfe <rowingwolfe@gmail.com>
2016-09-30 16:59:54 -0400

    Just fiddling around with the JS lab figuring out some algorithms. Going to spend some time on a bootcamp course to help me understand JS a bit better. Really need a better understanding to make these little projects nicer to look at and interact with. My issues with handling API responses must be resolved.

--- lab.js
+++ lab.js
@@ -84,3 +84,86 @@ getIndexToIns([40, 60], 50);
 */
 //EZPZ
 
+
+//Misread the instructions, kept returning the first divisor after 1.
+//I stole it again, I am a filthy, horrid man.
+//Though I believe the point is to have me thinking of algorithms to solve problems and I did... just solved the wrong problem. I do also understand how the code below works.
+/*function smallestCommons(arr) {
+    // Sort array from greater to lowest
+    // This line of code was from Adam Doyle (http://github.com/Adoyle2014)
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+
+    // Create new array and add all values from greater to smaller from the
+    // original array.
+    var newArr = [];
+    for (var i = arr[0]; i >= arr[1]; i--) {
+        newArr.push(i);
+    }
+
+    // Variables needed declared outside the loops.
+    var quot = 0;
+    var loop = 1;
+    var n;
+
+    // Run code while n is not the same as the array length.
+    do{
+        quot = newArr[0] * loop * newArr[1];
+        for (n = 2; n < newArr.length; n++) {
+            if (quot % newArr[n] !== 0) {
+                break;
+            }
+        }
+
+        loop++;
+    }while (n !== newArr.length);
+
+    return quot;
+}
+
+// test here
+console.log(smallestCommons([1,5]));*/
+
+/*function steamrollArray(arr) {
+    // I'm a steamroller, baby
+    var squish = [];
+    var roller = function(arg) {
+        if (!Array.isArray(arg)) {
+            squish.push(arg);
+        } else {
+            for (var a in arg) {
+                roller(arg[a]);
+            }
+        }
+    };
+    arr.forEach(roller);
+    return squish;
+}
+
+steamrollArray([1, [2], [3, [[4]]]]);*/
+/*
+function binaryAgent(str) {
+    binStr = str.split(' ');
+    engStr = [];
+    for(var i in binStr){
+        engStr.push(String.fromCharCode(parseInt(binStr[i], 2)));
+    }
+    return engStr.join('');
+}
+*/
+function addTogether(a,b) {
+    if (typeof a ==='number' && typeof b === 'number') {
+        return a + b;
+    }
+    else if (typeof a ==='number' && typeof b ==='undefined') {
+        return function(b) {
+            if (typeof b === 'object') {
+                return undefined;
+            } else {
+                return a + b;
+            }
+        };
+    }
+}
+addTogether(2, 3);
